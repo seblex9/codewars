56d50be17a77721a43d512bdfef305f55c966632
seb <seblex@proton.me>
2023-10-01 18:11:31 +0800

    add solution

---
+++ multiply.rb
@@ -0,0 +1,13 @@
+# This code does not execute properly. Try to figure out why.
+def multiply(a b)
+  a * b
+end
+
+# solution
+def multiply(a, b)
+  a * b
+end
+
+# tests
+multiply(1,1) # 1
+multiply(2,1) # 2
